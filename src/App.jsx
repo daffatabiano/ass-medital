213d0363cdaabb170f798a94983de89f871110aa
unknown <daffatabianoo@gmail.com>
2024-09-27 03:00:36 +0700

    feat(search): search bar features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
+import { FaMapMarkerAlt, FaSearch } from 'react-icons/fa';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <div className="search-box">
+        <i>
+          <FaMapMarkerAlt />
+        </i>
+        <input type="text" placeholder="Search some place" />
+        <button type="button">
+          <FaSearch />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
